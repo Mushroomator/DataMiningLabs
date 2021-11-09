4c7305f56578f82e6c0343da65029fc2faae1344
Mushroomator <ThomasPilz@gmx.de>
2021-11-09 16:15:33 +0100

    Synching work on laptop with PC

--- lab02/ex03.py
+++ lab02/ex03.py
@@ -1,20 +1,6 @@
 import numpy as np
 import pandas as pd
 
-births = pd.read_csv("C:\\Users\\topi5\\Documents\\Studium\\07_Semester\\Kapitel_01\\US_births_2000-2014.csv")
-
-# extract date components
-dateVals = births[["year", "month", "day_of_month"]]
-
-birthsPerWeekday = births[["day_of_week", "births"]].groupby("day_of_week").sum()
-
-# rename
-#dateVals.rename(columns={"day_of_month": "day"})
-# create new date column
-# birthsPerWeekday["date"] = pd.to_datetime(dateVals)
-
-print(birthsPerWeekday)
-
 def weekday(iday):
     idx2day = {
         "1": "Monday",
@@ -27,6 +13,18 @@ def weekday(iday):
     }
     return idx2day[str(iday)]
 
+births = pd.read_csv("C:\\Users\\topi5\\Documents\\Studium\\07_Semester\\Data_Mining\\Kapitel_01\\US_births_2000-2014.csv")
+# extract date components
+dateVals = births[["year", "month", "date_of_month"]].copy()
+# rename column so date can be created
+dateVals.rename(columns={"date_of_month": "day"}, inplace=True)
+# create new date column
+birthsPerWeekday = births[["day_of_week", "births"]].copy()
+result = birthsPerWeekday.groupby("day_of_week").sum()
+# result["day_of_week_str"] = result.apply(lambda it: weekday(it))
+print(result)
 
-idx = birthsPerWeekday.births.idxmax()
-print(f'Max births per weekday are {birthsPerWeekday.births[idx]} on a {weekday(idx)}')
+birthsPerWeekday["date"] = pd.to_datetime(dateVals)
+
+idx = result.births.idxmax()
+print(f'Max births per weekday are {result.iloc[idx].births} on a {weekday(result.iloc[idx].day_of_week)}')
